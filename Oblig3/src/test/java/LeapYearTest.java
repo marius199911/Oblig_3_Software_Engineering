e68d55e151cdaaa88de9e7272164a1a6528f4caa
marius199911 <mariuar@hiof.no>
2023-10-24 20:54:57 +0200

    Changes in pom.xml 2

--- Oblig3/src/test/java/LeapYearTest.java
+++ Oblig3/src/test/java/LeapYearTest.java
@@ -36,6 +36,6 @@ class LeapYearTest {
 
     @Test
     public void test_not_dividable_by_400(){
-        assertFalse(LeapYear.is_dividable_by_400(404));
+        assertFalse(LeapYear.is_dividable_by_400(408));
     }
 }
